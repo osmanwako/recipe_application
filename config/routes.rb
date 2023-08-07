b047a8ce7e1d5da95a5acfb9b0663b4ee0638afe
OsmanFP <osmanwako411@gmail.com>
2023-08-08 01:10:18 +0300

    Add Model and Update Migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "foods#index"
+  devise_for :users
+
+  resources :foods, only: [:index] do
+    resources :recipes, only: [:index]
+  end
+  resources :recipes, only: [:index] do
+    resources :foods, only: [:index]
+  end
 end
