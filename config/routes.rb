3028cb8abd16ec228520e6bfc524c8a2ef8e9b80
OsmanFP <osmanwako411@gmail.com>
2023-08-11 21:34:00 +0300

    Add new Recipe

--- config/routes.rb
+++ config/routes.rb
@@ -15,17 +15,6 @@ Rails.application.routes.draw do
   get "generate/recipe/:id", to: "recipes#generate"
   post "generate/recipe/:id/add", to: "recipes#add"
   patch "public/recipes/:id", to: "recipes#update"
-  devise_for :users,
-             path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'verification',
-                           registration: 'register' }
-  root 'foods#index'
-  resources :foods
-  resources :recipes, only: %i[index show destroy update] do
-    resources :recipefoods, only: [:index]
-  end
-  resources :recipefoods, only: [:index]
-  get 'public/recipes', to: 'recipes#public'
-  patch 'public/recipes/:id', to: 'recipes#update'
-  get 'general_shopping_list', to: 'shopping_lists#index'
+  get "general_shopping_list", to: "shopping_lists#index"
   resources :shopping_lists
 end
