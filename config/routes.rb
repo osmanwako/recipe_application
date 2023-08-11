d2f3e2651f36bfd5b9e7766e4b22c286cdec0023
Emmanuel <emmanuelnwoye12@gmail.com>
2023-08-11 08:59:29 +0100

    Add route for food resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout", password: "password", confirmation: "verification", registration: "register" }
   root "foods#index"
-  resources :foods, only: [:index] do
-    resources :recipefoods, only: [:index]
-  end
+  resources :foods
   resources :recipes, only: [:index, :show, :destroy, :update] do
     resources :recipefoods, only: [:index]
   end
